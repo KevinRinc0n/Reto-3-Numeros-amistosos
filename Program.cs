7ee16fd1eeb6d15c70ce6a6b41622436a9977468
KevinRinc0n <rinconkdavid@gmail.com>
2023-12-04 21:47:36 -0500

    feat: :sparkles: finished

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿for (int m = 1000; m <= 1500;m++)
+{
+    int n = amistoso(m);
+    if (n != m && amistoso(n) == m){
+        Console.WriteLine($"{m}, {n}");
+
+    }
+}
+
+static int amistoso (int numero){
+    int sum=  0;
+
+    for (int i = 1; i < numero; i++)
+    { 
+        if (numero % i == 0){
+            sum += i;
+        }
+    }
+    return sum;
+}
